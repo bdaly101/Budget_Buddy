5315f37d09ab72fbd06122d2cdae1dc540c746fc
Brendan <daly2b@gmail.com>
2024-02-14 12:48:46 -0500

    Fixed signup bugs

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,9 +1,8 @@
 import { useState } from 'react';
-import { Button, Checkbox, Label, TextInput, Alert } from 'flowbite-react';
-//import Link from 'next/link';
-import { ADD_USER } from '../utils/mutations';
+import { Button, Alert } from 'flowbite-react';
 import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
+import { Link, useNavigate } from 'react-router-dom';
 
 const SignupForm = () => {
 
@@ -53,60 +52,56 @@ const SignupForm = () => {
   };
 
   return (
-    <>
-      {/* This is needed for the validation functionality above */}
-      <form className="flex max-w-md flex-col gap-4" noValidate validated={validated} onSubmit={handleFormSubmit}>
-        {/* show alert if server response is bad */}
-        {/* <Alert color="failure" onDismiss={() => alert('Alert dismissed!')} icon={IoIosAlert}>
-          Something went wrong, please try again.
-        </Alert> */}
-        <Form.Group className='mb-3'>
-          <Form.Label htmlFor='username'>Username</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Your username'
-            name='username'
-            onChange={handleInputChange}
-            value={userFormData.username}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Username is required!</Form.Control.Feedback>
-        </Form.Group>
-
-        <Form.Group className='mb-3'>
-          <Form.Label htmlFor='email'>Email</Form.Label>
-          <Form.Control
-            type='email'
-            placeholder='Your email address'
-            name='email'
-            onChange={handleInputChange}
-            value={userFormData.email}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
-        </Form.Group>
-
-        <Form.Group className='mb-3'>
-          <Form.Label htmlFor='password'>Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='Your password'
-            name='password'
-            onChange={handleInputChange}
-            value={userFormData.password}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
-        </Form.Group>
-        <Button
-          disabled={!(userFormData.username && userFormData.email && userFormData.password)}
-          type='submit'
-          variant='success'>
-          Submit
-        </Button>
-      </form>
-    </>
+    <main className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 mt-6">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <img className="h-12 w-auto" src="/src/assets/images/webImages/BudgetBuddy-Logo.png" alt="Budget Buddy Logo" />
+        <h2 className="mt-10 text-center text-2xl leading-9 tracking-tight">Create your account</h2>
+      </div>
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <form className="space-y-6" noValidate validated={validated} onSubmit={handleFormSubmit}>
+          <div>
+            <input
+              type='text'
+              placeholder='Your username'
+              name='username'
+              onChange={handleInputChange}
+              value={userFormData.username}
+              required
+              className="block w-full rounded-md"
+            />
+          </div>
+          <div>
+            <input
+              type='email'
+              placeholder='Your email address'
+              name='email'
+              onChange={handleInputChange}
+              value={userFormData.email}
+              required
+              className="block w-full rounded-md"
+            />
+          </div>
+          <div>
+            <input
+              type='password'
+              placeholder='Your password'
+              name='password'
+              onChange={handleInputChange}
+              value={userFormData.password}
+              required
+              className="block w-full rounded-md"
+            />
+          </div>
+          <Button type="submit" className="w-full rounded-md bg-blue-500 text-gray-700">Submit</Button>
+        </form>
+        {error && <div className="mt-4 text-center text-sm text-red-600">{error.message}</div>}
+        <p className="mt-4 text-center">
+          Already have an account? <Link to="/login" className="text-indigo-600 hover:text-indigo-500">Sign in</Link>
+        </p>
+      </div>
+    </main>
   );
 };
+
 
 export default SignupForm;
