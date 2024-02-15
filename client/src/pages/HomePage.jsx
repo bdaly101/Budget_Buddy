44c10f3a25effcdee8fc61fe8def575052c156eb
Al Amin <al.amin003@hotmail.com>
2024-02-15 10:11:02 -0500

    added styling.

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import { useQuery } from '@apollo/client';
 import { QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth';
-import { useParams, Navigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 const Home = () => {
   const { username: userParam } = useParams();
@@ -11,21 +11,16 @@ const Home = () => {
   });
 
   const user = data?.me || data?.user || {};
-  console.log("Start")
-  console.log(data)
-  console.log(user);
-  console.log(user.username)
-  console.log("end")
+
   // navigate to personal profile page if username is yours
 
-  
   if (loading) {
-    return <div>Loading...</div>;
+    return <div className="text-center">Loading...</div>;
   }
 
   if (!user?.username) {
     return (
-      <h4>
+      <h4 className="text-center mt-8 text-red-500">
         You need to be logged in to see this. Use the navigation links above to
         sign up or log in!
       </h4>
@@ -33,16 +28,19 @@ const Home = () => {
   }
 
   if (Auth.loggedIn()) {
-
-    return (<>
-      <p>Viewing {user.username}'s profile</p>
-      <p>Money left in budget: {user.budget}</p>
-      <p>Top Expenses:</p>
-      <p>Recent Purchases:</p>
-    </>
+    return (
+      <div className="max-w-md mx-auto mt-8 p-4 bg-white rounded-md shadow-md">
+        <p className="text-lg font-semibold mb-2">
+          Viewing {user.username}'s profile
+        </p>
+        <p className="mb-2">
+          Money left in budget: ${user.budget}
+        </p>
+        <p className="mb-2">Top Expenses:</p>
+        <p className="mb-2">Recent Purchases:</p>
+      </div>
     );
   }
 };
 
 export default Home;
-
