558c5e25ab123147ea60c084245be97364b297fa
Brendan <daly2b@gmail.com>
2024-02-15 10:35:02 -0500

    Fixed bugs

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -10,36 +10,22 @@ export const QUERY_ME = gql`
       expenses {
         _id
         name
-        purchases {
-          _id
-          cost
-          createdAt
-        }
+        cost
+        createdAt
       }
     }
   }
 `;
 
-export const QUERY_PURCHASES = gql`
-  query purchases {
-    getPurchases {
-      _id
-      cost
-      createdAt
-    }
-  }
-`;
+
 
 export const QUERY_EXPENSES = gql`
   query expenses {
     getExpenses {
       _id
       name
-      purchases {
-        _id
-        cost
-        createdAt
-      }
+     cost
+     createdAt
     }
   }
 `;
@@ -49,22 +35,9 @@ export const QUERY_EXPENSE_BY_ID = gql`
     getExpenseById(id: $id) {
       _id
       name
-      purchases {
-        _id
-        cost
-        createdAt
-      }
-    }
-  }
-`;
-
-// Adding a query for a single purchase by its ID, assuming you need it based on your typeDefs
-export const QUERY_PURCHASE_BY_ID = gql`
-  query purchaseById($id: ID!) {
-    getPurchaseById(id: $id) {
-      _id
       cost
       createdAt
     }
   }
 `;
+
--- server/models/Expense.js
+++ server/models/Expense.js
@@ -10,38 +10,14 @@ const expenseSchema = new Schema({
   },
   // The purchases field is an array of ObjectId references to documents in the Purchase collection.
   // This creates a relationship between Expense documents and Purchase documents.
-  purchases: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Purchase', // Specifies that the ObjectId references the Purchase model.
-    },
-  ],
-}, {
-  toJSON: { virtuals: true }, // Enable virtuals when document is converted to JSON
-  toObject: { virtuals: true }, // Enable virtuals when document is converted to a plain object
+  cost: {
+    type: Number,
+    required: true, // Assuming every purchase must have a cost
+    
+  },
 });
 // Static method to calculate total cost
-expenseSchema.statics.calculateTotalCost = async function(expenseId) {
-  const result = await this.aggregate([
-    { $match: { _id: expenseId } },
-    { $lookup: {
-        from: 'purchases', 
-        localField: 'purchases',
-        foreignField: '_id',
-        as: 'purchaseDetails'
-      }
-    },
-    { $unwind: '$purchaseDetails' },
-    { $group: {
-        _id: '$_id',
-        totalCost: { $sum: '$purchaseDetails.cost' },
-        name: { $first: '$name' }
-      }
-    }
-  ]);
 
-  return result.length > 0 ? result[0] : null;
-};
 
 const Expense = model('Expense', expenseSchema);
 
